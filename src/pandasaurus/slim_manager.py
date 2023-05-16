710e8c4c0f0535bde821512361a240dede12f73a
Ugur Bayindir <ubyndr@gmail.com>
2023-05-16 08:34:23 +0100

    Clean up

--- src/pandasaurus/slim_manager.py
+++ src/pandasaurus/slim_manager.py
@@ -1,7 +1,6 @@
 from typing import Dict, List
 
 from .resources.slim import Slim
-from .resources.term import Term
 from .utils.query_utils import run_sparql_query
 from .utils.sparql_queries import get_slim_list_query, get_slim_members_query
 
@@ -28,8 +27,6 @@ class SlimManager:
             slim_list.update(
                 {res.get("label"): Slim(name=res.get("label"), description=res.get("comment"))}
             )
-        # slim_list = slim_list if not slim_list else slim_list.update(slim_list)
-        # We probably need a good print method here instead of returning a list of list here.
         return [
             {"name": slim.get_name(), "description": slim.get_description()}
             for slim in slim_list.values()
@@ -46,11 +43,8 @@ class SlimManager:
             Term IRIs of the slim members
 
         """
-        # We probably need a good print method here instead of returning a list of list here.
-
         return [
             term.get("term")
             for slim_name in slim_list
             for term in run_sparql_query(get_slim_members_query(slim_name))
         ]
-        # return [term.get("term") for term in run_sparql_query(get_slim_members_query(slim_name))]
