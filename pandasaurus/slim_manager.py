8f1a8c0bb202ef1bfa4fbcbc3c36fcfbce048364
Ugur Bayindir <ubyndr@gmail.com>
2023-06-26 15:04:23 +0100

    Added ontology name validation in get_slim_list

--- pandasaurus/slim_manager.py
+++ pandasaurus/slim_manager.py
@@ -1,8 +1,13 @@
 from typing import Dict, List
 
+from pandasaurus.utils.pandasaurus_exceptions import InvalidOntology
 from pandasaurus.resources.slim import Slim
 from pandasaurus.utils.query_utils import run_sparql_query
-from pandasaurus.utils.sparql_queries import get_slim_list_query, get_slim_members_query
+from pandasaurus.utils.sparql_queries import (
+    get_ontology_list_query,
+    get_slim_list_query,
+    get_slim_members_query,
+)
 
 
 class SlimManager:
@@ -22,11 +27,14 @@ class SlimManager:
             Slim names and definitions list
 
         """
-        slim_list: Dict[str, Slim] = dict()
-        result = run_sparql_query(get_slim_list_query(ontology))
-        for res in result:
-            slim_list.update({res.get("label"): Slim(name=res.get("label"), description=res.get("comment"))})
-        return [{"name": slim.get_name(), "description": slim.get_description()} for slim in slim_list.values()]
+        ontology_list = SlimManager._get_ontology_list()
+        if ontology in ontology_list:
+            slim_list: Dict[str, Slim] = dict()
+            result = run_sparql_query(get_slim_list_query(ontology))
+            for res in result:
+                slim_list.update({res.get("label"): Slim(name=res.get("label"), description=res.get("comment"))})
+            return [{"name": slim.get_name(), "description": slim.get_description()} for slim in slim_list.values()]
+        raise InvalidOntology(ontology, ontology_list)
 
     @staticmethod
     def get_slim_members(slim_list: List[str]) -> List[str]:
@@ -42,3 +50,7 @@ class SlimManager:
         return [
             term.get("term") for slim_name in slim_list for term in run_sparql_query(get_slim_members_query(slim_name))
         ]
+
+    @staticmethod
+    def _get_ontology_list():
+        return [row.get("title") for row in run_sparql_query(get_ontology_list_query())]
