19f08ed715af9b7d132d4dabe860a32712efbdf0
Ugur Bayindir <ubyndr@gmail.com>
2023-08-24 17:41:03 +0100

    Added docstrings in graph_generator.py

--- pandasaurus/graph/graph_generator.py
+++ pandasaurus/graph/graph_generator.py
@@ -18,6 +18,19 @@ logger = configure_logger()
 class GraphGenerator:
     @staticmethod
     def generate_enrichment_graph(enriched_df: pd.DataFrame) -> Graph:
+        """
+            Generates an RDF graph representing enrichment relationships from an enriched DataFrame.
+
+            Args:
+                enriched_df (pd.DataFrame): A DataFrame containing enrichment relationships.
+                    It should have columns 's', 's_label', 'o', and 'o_label' representing subjects,
+                    subject labels, objects, and object labels respectively.
+
+            Returns:
+                Graph: An RDF graph representing the enrichment relationships.
+                    Each subject is linked to its corresponding object using the 'subClassOf' relationship,
+                    and labels are associated with subjects and objects using the 'label' relationship.
+        """
         graph = Graph()
         cl_namespace = Namespace("http://purl.obolibrary.org/obo/CL_")
         for _, row in enriched_df.iterrows():
@@ -30,6 +43,25 @@ class GraphGenerator:
 
     @staticmethod
     def apply_transitive_reduction(graph: Graph, predicate_list: List[str]) -> Graph:
+        """
+            Applies transitive reduction to a given RDF graph using specified predicates.
+
+            Args:
+                graph (Graph): The RDF graph to which transitive reduction is applied.
+                predicate_list (List[str]): A list of predicates (URIs or 'rdfs:subClassOf')
+                    to which transitive reduction will be applied.
+
+            Returns:
+                Graph: The RDF graph after applying transitive reduction to the specified predicates.
+
+            Note:
+                - The method applies transitive reduction to the specified predicates by adding only the necessary edges
+                  to create a directed acyclic graph.
+                - Redundant triples are removed from the graph using transitive reduction results.
+                - The 'predicate_list' should contain valid predicates existing in the graph.
+                - For 'predicate_list', you can use either full URIs or 'rdfs:subClassOf' for the RDF schema 'subClassOf'
+                  relationship.
+        """
         invalid_predicates = []
         # TODO We need a better way to handle the queries, and decide the format we accept in the predicate list
         ask_query = prepareQuery("SELECT ?s ?p WHERE { ?s ?p ?o }")
